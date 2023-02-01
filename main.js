eb23755b3ebf985475212d9cb50b5cb8c94a5300
KhuongNC <khuongnc@rikkeisoft.com>
2023-02-01 15:02:48 +0700

    Add script to play audio automatically

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-let audio = document.getElementsByClassName("song")[0];
-audio.play();
+// Wrap the native DOM audio element play function and handle any autoplay errors
+Audio.prototype.play = (function (play) {
+  return function () {
+    var audio = this,
+      args = arguments,
+      promise = play.apply(audio, args);
+
+    if (promise !== undefined) {
+      promise.catch((_) => {
+        // Autoplay was prevented. This is optional, but add a button to start playing.
+        var el = document.createElement("button");
+        el.innerHTML = "Play";
+        el.addEventListener("click", function () {
+          play.apply(audio, args);
+        });
+        this.parentNode.insertBefore(el, this.nextSibling);
+      });
+    }
+  };
+})(Audio.prototype.play);
+
+// Try automatically playing our audio via script. This would normally trigger and error.
+document.getElementsByClassName("song")[0].play();
 
 // Animation timeline
 const animationTimeline = () => {
